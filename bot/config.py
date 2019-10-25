18fce5b20c6a35596644801839e87f10a78e82c7
SunnyCapt <42912268+SunnyCapt@users.noreply.github.com>
2019-10-25 22:26:22 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ sp_data_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sp.
 bot_token: str = None
 students: List = None
 teacher_ids: List = None
-proxy: str = "protocol://host:port"
+proxy: str = None  # "protocol://host:port"
 
 try:
     from .local_config import *
