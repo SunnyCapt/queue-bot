b1190f973be002af21aa47743e88480b7005fc07
SunnyCapt <42912268+SunnyCapt@users.noreply.github.com>
2019-10-25 22:21:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-mkdir "queue-bot"
-mv bot Procfile requirements.txt "queue-bot"
+
+mkdir queue-bot
+mv `ls -a | egrep -v '^(\.|queue-bot|\.\.)$'` queue-bot
 cd queue-bot
-heroku create queue-bot
-git init && echo "git inited"
-heroku git:remote -a queue-bot
+read bot_name
+heroku create $bot_name
+git init && echo 'git inited'
+heroku git:remote -a $bot_name
 git add . && git commit -am "init" && git push heroku master && echo "run.."
 heroku ps:scale bot=1
 #heroku logs -t
